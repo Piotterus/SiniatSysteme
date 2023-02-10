9a0b05593cdb6d18d359e2944d06f34a14b9335d
Piotr_Marcin Katański <piotr_marcin.katanski@student.put.poznan.pl>
2023-02-10 16:46:34 +0100

    obsługa kroku 0

--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -17,27 +17,27 @@ const HomeScreen = () => {
       <MainMenuButton
         image={icons.mainMenu.wandeColor}
         text={'Wandsysteme - Schachtwände -Vorsatzschalen - Trockenputz'}
-        onPress={() => navigation.navigate('Stage1', {})}
+        onPress={() => navigation.navigate('Stage1', {step1: 'wande'})}
       />
       <MainMenuButton
         image={icons.mainMenu.deckenColor}
         text={'Deckensysteme'}
-        onPress={() => navigation.navigate('Stage1')}
+        onPress={() => navigation.navigate('Stage1', {step1: 'decken'})}
       />
       <MainMenuButton
         image={icons.mainMenu.dacherColor}
         text={'Dachsysteme'}
-        onPress={() => navigation.navigate('Stage1')}
+        onPress={() => navigation.navigate('Stage1', {step1: 'dacher'})}
       />
       <MainMenuButton
         image={icons.mainMenu.stutzenColor}
         text={'Stützen - und Trägerbekleidungen'}
-        onPress={() => navigation.navigate('Stage1')}
+        onPress={() => navigation.navigate('Stage1', {step1: 'stutzen'})}
       />
       <MainMenuButton
         image={icons.mainMenu.kabelkanaleColor}
         text={'Kabelkanäle'}
-        onPress={() => navigation.navigate('Stage1')}
+        onPress={() => navigation.navigate('Stage1', {step1: 'kabelkanale'})}
       />
     </CustomBackground>
   );
--- src/screens/stages/Stage1Screen.js
+++ src/screens/stages/Stage1Screen.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useCallback, useState} from 'react';
 import CustomBackground from '../../components/backgrounds/CustomBackground';
 import {Image, Text, View} from 'react-native';
 import icons from '../../assets/icons';
@@ -8,9 +8,104 @@ import styles from './StagesScreen.style';
 import {WhiteButton} from '../../components/buttons/CustomButton';
 import {FullModal, Stage1Modal} from '../../components/modals/CustomModals';
 import {SystemHeader} from '../../components/headers/CustomHeaders';
+import {
+  useFocusEffect,
+  useNavigation,
+  useRoute,
+} from '@react-navigation/native';
 
 const Stage1Screen = () => {
   const [stageModalVisible, setStageModalVisible] = useState(false);
+  const [step1, setStep1] = useState();
+  const optionList = {
+    wande: [
+      {
+        label: 'Metallständerwände',
+        value: 'metallstanderwande',
+      },
+      {
+        label:
+          '"Brandwände" Nichtragende raumabschliessende Wände mit prüftechnisch nachgewiesener Stoßbeanspruchung',
+        value: 'brandwande',
+      },
+      {
+        label: 'Holzständerwände',
+        value: 'holzstanderwande',
+      },
+      {
+        label: 'Schachtwände/Vorsatzschalen',
+        value: 'schachtwande-vorsatzschalen',
+      },
+      {
+        label: 'Trockenputz',
+        value: 'trockenputz',
+      },
+    ],
+    decken: [
+      {
+        label: 'Freitragende Unterdecken',
+        value: 'freitragende-unterdecken',
+      },
+      {
+        label: 'Unterdecken und Deckenbekleidungen',
+        value: 'unterdecken-und-deckenbekleidungen',
+      },
+      {
+        label: 'Ertüchtigung Massivdecken',
+        value: 'ertuchtigung-massivdecken',
+      },
+      {
+        label: 'Ertüchtigung Holzbalkendecken',
+        value: 'ertuchtigung-holzbalkendecken',
+      },
+      {
+        label: 'Ertüchtigung Trapezblechdecken',
+        value: 'trapezblechdecken',
+      },
+    ],
+    dacher: [
+      {
+        label: 'Holzbalkendächer',
+        value: 'dacher',
+      },
+      {
+        label: 'Trapezblechdächer Ertüchtigung Trapezblechdächer',
+        value: 'trapezblechdacher-ertuchtigung-trapezblechdacher',
+      },
+    ],
+    stutzen: [
+      {
+        label: 'Stahl',
+        value: 'stahl',
+      },
+      {
+        label: 'Holz',
+        value: 'holz',
+      },
+    ],
+    kabelkanale: [
+      {
+        label: 'E-Kanäle',
+        value: 'e-kanale',
+      },
+      {
+        label: 'I-Kanäle',
+        value: 'i-kanale',
+      },
+    ],
+  };
+
+  const route = useRoute();
+  const navigation = useNavigation();
+  useFocusEffect(
+    useCallback(() => {
+      setStep1(route.params.step1);
+
+      return () => setStep1(undefined);
+    }, [route.params]),
+  );
+
+  console.log(optionList[step1]);
   return (
     <CustomBackground header={'siniat'}>
       <Stage1Modal
@@ -19,7 +114,17 @@ const Stage1Screen = () => {
       />
       <SystemHeader />
       <StageNavigation />
-      <WhiteButton
+      {optionList[step1] !== undefined &&
+        optionList[step1].map((item, index) => {
+          return (
+            <WhiteButton
+              text={item.label}
+              onPress={() => setStageModalVisible(true)}
+              key={index}
+            />
+          );
+        })}
+      {/*<WhiteButton
         text={'Freicośtam'}
         onPress={() => setStageModalVisible(true)}
       />
@@ -38,7 +143,7 @@ const Stage1Screen = () => {
       <WhiteButton
         text={'Freicośtam'}
         onPress={() => setStageModalVisible(true)}
-      />
+      />*/}
     </CustomBackground>
   );
 };
