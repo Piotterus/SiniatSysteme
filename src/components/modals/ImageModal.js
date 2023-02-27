c26e2753b409dca354f0822a0197b84be9d37338
Piotr_Marcin Katański <piotr_marcin.katanski@student.put.poznan.pl>
2023-02-27 11:44:19 +0100

    powiększanie zdjęć poprawka ios

--- src/components/modals/ImageModal.js
+++ src/components/modals/ImageModal.js
@@ -6,6 +6,7 @@ import {
   Dimensions,
   Image,
   SafeAreaView,
+  View,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import ImageZoom from 'react-native-image-pan-zoom';
@@ -18,31 +19,33 @@ const ImageModal = props => {
       style={styles.imageModal.mainView}
       onBackdropPress={() => props.setVisible(false)}>
       <SafeAreaView style={styles.imageModal.mainView}>
-        <TouchableOpacity
-          style={{
-            position: 'absolute',
-            top: 20,
-            right: 30,
-            zIndex: 3,
-            elevation: 3,
-          }}
-          onPress={() => props.setVisible(false)}>
-          <Icon name="x-circle" size={50} color="#FFFFFF" />
-        </TouchableOpacity>
-        <ImageZoom
-          cropWidth={Dimensions.get('window').width}
-          cropHeight={Dimensions.get('window').height}
-          imageWidth={Dimensions.get('window').width * 0.6}
-          imageHeight={Dimensions.get('window').height * 0.6}>
-          <Image
+        <View style={styles.imageModal.mainView}>
+          <TouchableOpacity
             style={{
-              width: Dimensions.get('window').width * 0.6,
-              height: Dimensions.get('window').height * 0.6,
+              position: 'absolute',
+              top: 20,
+              right: 30,
+              zIndex: 3,
+              elevation: 3,
             }}
-            source={{uri: props.imageModal}}
-            resizeMode={'contain'}
-          />
-        </ImageZoom>
+            onPress={() => props.setVisible(false)}>
+            <Icon name="x-circle" size={50} color="#FFFFFF" />
+          </TouchableOpacity>
+          <ImageZoom
+            cropWidth={Dimensions.get('window').width}
+            cropHeight={Dimensions.get('window').height}
+            imageWidth={Dimensions.get('window').width * 0.6}
+            imageHeight={Dimensions.get('window').height * 0.6}>
+            <Image
+              style={{
+                width: Dimensions.get('window').width * 0.6,
+                height: Dimensions.get('window').height * 0.6,
+              }}
+              source={{uri: props.imageModal}}
+              resizeMode={'contain'}
+            />
+          </ImageZoom>
+        </View>
       </SafeAreaView>
     </Modal>
   );
