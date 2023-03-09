4bacaedc900e0fff216f465f49775a246928cb9d
Piotr_Marcin Katański <piotr_marcin.katanski@student.put.poznan.pl>
2023-03-09 15:22:38 +0100

    header w webview

--- src/screens/singlePage/AboutScreen.js
+++ src/screens/singlePage/AboutScreen.js
@@ -3,12 +3,14 @@ import WebView from 'react-native-webview';
 import CustomFooter from '../../components/footers/CustomFooter';
 import ApiContext from '../../contexts/ApiContext';
 import {SafeAreaView} from 'react-native';
+import {SiniatHeader} from '../../components/headers/CustomHeaders';
 
 const AboutScreen = () => {
   const {siteUrl} = useContext(ApiContext);
 
   return (
     <SafeAreaView style={{flex: 1}}>
+      <SiniatHeader />
       <WebView
         style={{flex: 1}}
         source={{uri: siteUrl + 'about'}}
