a1c05340295e877fe2f675c429a37ee838f9810c
Piotr_Marcin Katański <piotr_marcin.katanski@student.put.poznan.pl>
2023-03-27 11:40:25 +0200

    strony weview

--- src/screens/singlePage/PallasScreen.js
+++ src/screens/singlePage/PallasScreen.js
@@ -2,20 +2,25 @@ import React, {useContext} from 'react';
 import WebView from 'react-native-webview';
 import CustomFooter from '../../components/footers/CustomFooter';
 import ApiContext from '../../contexts/ApiContext';
-import {SafeAreaView} from 'react-native';
+import {Platform, SafeAreaView} from 'react-native';
 import {SiniatHeader} from '../../components/headers/CustomHeaders';
 import {ScreenWidth} from '@rneui/base';
 
 const PallasScreen = () => {
   const {siteUrl} = useContext(ApiContext);
 
+  const INJECTEDJAVASCRIPT = `const meta = document.createElement('meta'); meta.setAttribute('content', 'initial-scale=1.0, maximum-scale=1.0, user-scalable=0'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta); `;
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <SiniatHeader />
       <WebView
         style={{flex: 1, width: ScreenWidth}}
+        scalesPageToFit={Platform.OS !== 'android'}
         source={{uri: siteUrl + 'pallas'}}
+        injectedJavaScript={INJECTEDJAVASCRIPT}
         cacheEnabled={false}
+        useWebkit={false}
       />
       <CustomFooter />
     </SafeAreaView>
