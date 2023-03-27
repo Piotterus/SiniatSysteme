c307d5a7dac997ed7abbceea1893d26ea1f43e97
Piotr_Marcin Katański <piotr_marcin.katanski@student.put.poznan.pl>
2023-03-27 11:07:40 +0200

    kontakt ios

--- src/screens/singlePage/ContactScreen.js
+++ src/screens/singlePage/ContactScreen.js
@@ -9,7 +9,7 @@ import {ScreenWidth} from '@rneui/base';
 const ContactScreen = () => {
   const {siteUrl} = useContext(ApiContext);
 
-  const INJECTEDJAVASCRIPT = `const meta = document.createElement('meta'); meta.setAttribute('content', 'initial-scale=1.0, maximum-scale=1.0, user-scalable=0'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta); `;
+  const INJECTEDJAVASCRIPT = `const meta = document.createElement('meta'); meta.setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta); `;
 
   return (
     <SafeAreaView style={{flex: 1}}>
