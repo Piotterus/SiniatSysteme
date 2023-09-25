11972602af82771cbf2df339d0a072c8d8091d70
Piotr_Marcin Katański <piotr_marcin.katanski@student.put.poznan.pl>
2023-09-25 10:10:56 +0200

    odświeżanie easyboarda

--- src/screens/singlePage/EasyboardScreen.js
+++ src/screens/singlePage/EasyboardScreen.js
@@ -1,31 +1,44 @@
-import React, {useContext} from 'react';
+import React, {useCallback, useContext, useRef, useState} from 'react';
 import WebView from 'react-native-webview';
 import CustomFooter from '../../components/footers/CustomFooter';
 import ApiContext from '../../contexts/ApiContext';
-import {Platform, SafeAreaView} from 'react-native';
+import {Platform, SafeAreaView, View} from 'react-native';
 import {SiniatHeader} from '../../components/headers/CustomHeaders';
 import {ScreenWidth} from '@rneui/base';
 import LanguageContext from '../../contexts/LanguageContext';
+import {useFocusEffect} from '@react-navigation/native';
 
 const EasyboardScreen = () => {
   const {siteUrl} = useContext(ApiContext);
   const {activeLanguageCode} = useContext(LanguageContext);
+  const [loading, setLoading] = useState(true);
 
   const INJECTEDJAVASCRIPT = `const meta = document.createElement('meta'); meta.setAttribute('content', 'initial-scale=1.0, maximum-scale=1.0, user-scalable=0'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta); `;
 
   let uri = siteUrl + 'easyboard';
 
+  useFocusEffect(
+    useCallback(() => {
+      setLoading(false);
+      return () => setLoading(true);
+    }, []),
+  );
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <SiniatHeader />
-      <WebView
-        style={{flex: 1, width: ScreenWidth}}
-        scalesPageToFit={Platform.OS !== 'android'}
-        source={{uri: uri}}
-        injectedJavaScript={INJECTEDJAVASCRIPT}
-        cacheEnabled={false}
-        useWebkit={false}
-      />
+      {loading === true && <View style={{flex: 1}} />}
+      {loading === false && (
+        <WebView
+          style={{flex: 1, width: ScreenWidth}}
+          scalesPageToFit={Platform.OS !== 'android'}
+          source={{uri: uri}}
+          injectedJavaScript={INJECTEDJAVASCRIPT}
+          cacheEnabled={false}
+          useWebkit={false}
+          startInLoadingState={true}
+        />
+      )}
       <CustomFooter />
     </SafeAreaView>
   );
