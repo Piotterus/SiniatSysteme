66343482981a66815d7df8971bc8b27de6c49ecd
Piotr_Marcin Katański <piotr_marcin.katanski@student.put.poznan.pl>
2023-09-26 10:50:42 +0200

    zmiana linka easyboarda

--- src/screens/singlePage/EasyboardScreen.js
+++ src/screens/singlePage/EasyboardScreen.js
@@ -15,7 +15,7 @@ const EasyboardScreen = () => {
 
   const INJECTEDJAVASCRIPT = `const meta = document.createElement('meta'); meta.setAttribute('content', 'initial-scale=1.0, maximum-scale=1.0, user-scalable=0'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta); `;
 
-  let uri = siteUrl + 'easyboard';
+  let uri = siteUrl + 'easyboard/app';
 
   useFocusEffect(
     useCallback(() => {
